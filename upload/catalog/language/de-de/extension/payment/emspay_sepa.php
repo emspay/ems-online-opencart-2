881ff76b54bf179f16feefe4920245739bc6c794
Maksim <maxim.volgust@leasoft.org>
2019-07-31 07:36:40 +0200

    Catalog Language changes

--- upload/catalog/language/de-de/extension/payment/emspay_sepa.php
+++ upload/catalog/language/de-de/extension/payment/emspay_sepa.php
@@ -9,11 +9,11 @@ $_['text_transaction'] = 'Bezahlung: ';
 $_['text_description'] = "Bitte überweisen Sie den Gesamtbetrag auf folgendes Bankkonto:";
 $_['button_confirm'] = 'Bezahlung bestätigen';
 
-$_['ing_bank_details'] = 'Bitte verwenden Sie folgende Bankkontodaten: ';
-$_['ing_payment_reference'] = 'Zahlungsreferenz: ';
-$_['ing_iban'] =  'IBAN: ';
-$_['ing_bic'] = 'BIC: ';
-$_['ing_account_holder'] = 'Kontoinhaber: ';
-$_['ing_residence'] = 'Wohnort: ';
+$_['ems_bank_details'] = 'Bitte verwenden Sie folgende Bankkontodaten: ';
+$_['ems_payment_reference'] = 'Zahlungsreferenz: ';
+$_['ems_iban'] =  'IBAN: ';
+$_['ems_bic'] = 'BIC: ';
+$_['ems_account_holder'] = 'Kontoinhaber: ';
+$_['ems_residence'] = 'Wohnort: ';
 
 $_['error_another_payment_method'] = "<br/>Leider ist ein Fehler bei der Verarbeitung Ihrer Bezahlung aufgetreten. Wir bitten um Entschuldigung für jegliche Unannehmlichkeiten. Bitte eine andere Zahlungsart zu wahlen.";
--- upload/catalog/language/nl-nl/extension/payment/emspay_sepa.php
+++ upload/catalog/language/nl-nl/extension/payment/emspay_sepa.php
@@ -9,11 +9,11 @@ $_['text_transaction'] = 'Betaling: ';
 $_['text_description'] = 'Wilt u het totaalbedrag overmaken met de volgende gegevens  :';
 $_['button_confirm'] = 'Betaling bevestigen';
 
-$_['ing_bank_details'] = 'Gebruikt u de volgende gegevens voor uw overboeking: ';
-$_['ing_payment_reference'] = 'Betalingskenmerk: ';
-$_['ing_iban'] =  'IBAN: ';
-$_['ing_bic'] = 'BIC: ';
-$_['ing_account_holder'] = 'Rekeninghouder: ';
-$_['ing_residence'] = 'Woonplaats: ';
+$_['ems_bank_details'] = 'Gebruikt u de volgende gegevens voor uw overboeking: ';
+$_['ems_payment_reference'] = 'Betalingskenmerk: ';
+$_['ems_iban'] =  'IBAN: ';
+$_['ems_bic'] = 'BIC: ';
+$_['ems_account_holder'] = 'Rekeninghouder: ';
+$_['ems_residence'] = 'Woonplaats: ';
 
 $_['error_another_payment_method'] = "<br/>Helaas is er een fout opgetreden tijdens het verwerken van uw betaling met de gekozen payment methode. Onze excuses voor het ongemak. Kies een andere betaalmethode AUB.";
