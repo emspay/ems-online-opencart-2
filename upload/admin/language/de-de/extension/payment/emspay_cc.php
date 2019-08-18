cc5ce9e395985a1f985b9cbfdaeee7af6a98d28f
Maksim <maxim.volgust@leasoft.org>
2019-08-18 18:40:19 +0200

    Changed to EMS Online in the Admin language

--- upload/admin/language/de-de/extension/payment/emspay_cc.php
+++ upload/admin/language/de-de/extension/payment/emspay_cc.php
@@ -3,8 +3,8 @@
 /**
  * General Admin Settings Page
  */
-$_['heading_title'] = 'EMS PAY: Kreditkarte';
-$_['text_emspay_cc'] = '<img src="view/image/payment/emspay_creditcard.png" alt="EMS PAY" title="EMS PAY" />';
+$_['heading_title'] = 'EMS Online: Kreditkarte';
+$_['text_emspay_cc'] = '<img src="view/image/payment/emspay_creditcard.png" alt="EMS Online" title="EMS Online" />';
 $_['text_extension'] = 'Extensions';
 
 /**
@@ -18,7 +18,7 @@ $_['entry_order_cancelled'] = 'Bestellung annulliert:';
 $_['entry_order_processing'] = 'Bestellung wird bearbeitet:';
 $_['entry_order_error'] = 'Bestellfehler:';
 $_['entry_sort_order'] = 'Sortierreihenfolge:';
-$_['entry_ems_api_key'] = 'EMS PAY API Schüssel:';
+$_['entry_ems_api_key'] = 'EMS Online API Schüssel:';
 $_['entry_ems_total'] = 'Gesamt:';
 $_['entry_cacert'] = 'Bundle cURL ca.cert:';
 $_['entry_send_webhook'] = 'Webhook-URL automatisch generieren:';
@@ -33,22 +33,22 @@ $_['text_enabled'] = 'Aktiviert';
 $_['text_disabled'] = 'Deaktiviert';
 $_['text_payments'] = 'Zahlungen';
 $_['text_issuer_id'] = 'SWIFT/BIC';
-$_['text_settings_saved'] = 'EMS PAY: Kreditkarten Einstellungen aktualisiert!';
-$_['text_edit_ems'] = 'Bearbeiten EMS PAY: Kreditkarte Einstellungen';
+$_['text_settings_saved'] = 'EMS Online: Kreditkarten Einstellungen aktualisiert!';
+$_['text_edit_ems'] = 'Bearbeiten EMS Online: Kreditkarte Einstellungen';
 $_['text_yes'] = 'Ja';
 $_['text_no'] = 'Nein';
 
 /**
  * Error messages
  */
-$_['error_missing_api_key'] = 'EMS PAY API schlüssel ist erforderlich!';
+$_['error_missing_api_key'] = 'EMS Online API schlüssel ist erforderlich!';
 
 /**
  * Information text
  */
-$_['info_help_api_key'] = 'Duplizieren Sie Ihre EMS PAY API Schlüssel von Merchant Portal.';
+$_['info_help_api_key'] = 'Duplizieren Sie Ihre EMS Online API Schlüssel von Merchant Portal.';
 $_['info_help_total'] = 'Die Zahlungsmethode wird gezeigt wenn die kasse insgesamt der Mindestbetrag erreicht.';
-$_['info_plugin_not_configured'] = 'EMS PAY: Kreditkarten plugin ist nicht konfiguriert.';
+$_['info_plugin_not_configured'] = 'EMS Online: Kreditkarten plugin ist nicht konfiguriert.';
 $_['info_help_klarna_ip_filter'] = 'Wenn sie eingegeben werden, wird nur für diese IPs die Zahlungsmethode angezeigt. (Beispiel: 128.0.0.1, 255.255.255.255)';
 $_['entry_klarna_ip_filter'] = 'IP-Filterung:';
 $_['entry_klarna_test_api_key'] = 'Test API schlüssel:';
