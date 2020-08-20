e19e51ba2cd96416a20e639352c3b8bd7ea36dfb
Shapoval.Konstantin <konstantin.shapoval@leasoft.org>
2020-08-20 12:30:26 +0300

    Klarna-Pay-Later: Remove fields gender and birthday from checkout form and customer object

--- upload/catalog/language/fr-fr/extension/payment/emspay_klarnapaylater.php
+++ upload/catalog/language/fr-fr/extension/payment/emspay_klarnapaylater.php
@@ -16,10 +16,3 @@ $_['pending_message'] = 'Nous avons pas reçu la confirmation de votre banque ou
 $_['pending_message_sub'] = 'Vous recevrez un message dès que nous recevrons ceci.';
 
 $_['error_another_payment_method'] = "<br/>Il y avait une erreur lors du traitement de votre commande avec le methode de paiment que vous avez choisi. Nous nous excusons pour le dérangement. Veuillez choisir une autre méthode de paiement.";
-
-$_['text_please_enter_dob'] = 'Veuillez entrez votre date de naissance';
-$_['text_please_select_gender'] = 'Sexe';
-$_['text_please_select_gender_male'] = 'Homme';
-$_['text_please_select_gender_female'] = 'Femme';
-
-$_['error_invalid_dob'] = 'Il y avait une erreur avec la date de naissance que vous avez utilisée. Veuillez utiliser le format suivant pour entrer la date de naissance : JJ-MM-AAAA; par ex: 31-12-1980.';
--- upload/catalog/language/nl-nl/extension/payment/emspay_klarnapaylater.php
+++ upload/catalog/language/nl-nl/extension/payment/emspay_klarnapaylater.php
@@ -16,11 +16,3 @@ $_['pending_message'] = 'We hebben nog geen bevestiging van uw bank of kaartuitg
 $_['pending_message_sub'] = 'U ontvangt een bericht zodra we deze hebben ontvangen.';
 
 $_['error_another_payment_method'] = "<br/>Helaas is er een fout opgetreden tijdens het verwerken van uw betaling met de gekozen payment methode. Onze excuses voor het ongemak. Kies een andere betaalmethode AUB.";
-
-$_['text_please_enter_dob'] = 'Geboortedatum';
-$_['text_please_select_gender'] = 'Geslacht';
-$_['text_please_select_gender_male'] = 'Man';
-$_['text_please_select_gender_female'] = 'Vrouw';
-
-$_['error_invalid_dob'] = 'Er is een fout opgetreden met de geboortedatum die u heeft opgegeven. Gebruik het volgende formaat voor het invoeren van de geboortedatum: DD-MM-JJJJ; bijv: 31-12-1980.';
-
